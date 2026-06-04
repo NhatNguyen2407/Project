69c6cedf928ec060719ebb79e92b66a80376f7ec
Nguyen Huyen Minh Nhat <john.bibibi@gmail.com>
2026-06-04 19:56:20 +0700

    Update HomePage.jsx

--- Working_Projects/Dioxyzine_Frog/src/app/pages/HomePage.jsx
+++ Working_Projects/Dioxyzine_Frog/src/app/pages/HomePage.jsx
@@ -102,7 +102,7 @@ export function HomePage() {
                         <a href="https://facebook.com/dioxyzinefrog" target="_blank" rel="noopener noreferrer" className="w-10 h-10 rounded-full bg-[#1877F2] hover:scale-110 text-white flex items-center justify-center transition-transform shadow-md">
                           <Facebook className="w-5 h-5" />
                         </a>
-                        <a href="https://instagram.com/dioxyzine.frog" target="_blank" rel="noopener noreferrer" className="w-10 h-10 rounded-full bg-gradient-to-r from-[#F58529] via-[#DD2A7B] to-[#8134AF] hover:scale-110 text-white flex items-center justify-center transition-transform shadow-md">
+                        <a href="https://instagram.com/dioxyzinefrog.print" target="_blank" rel="noopener noreferrer" className="w-10 h-10 rounded-full bg-gradient-to-r from-[#F58529] via-[#DD2A7B] to-[#8134AF] hover:scale-110 text-white flex items-center justify-center transition-transform shadow-md">
                           <Instagram className="w-5 h-5" />
                         </a>
                       </div>
